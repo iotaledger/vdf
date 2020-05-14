e73f47268b1bd41c1f1f9eb2bd233fc65a353e59
Vidal Attias <vidal.attias@gmail.com>
2020-05-14 20:15:43 +0200

    ok

--- scripts/verification.py
+++ scripts/verification.py
@@ -1,10 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-t_values = range(20, 25)
+t_values = range(10, 15)
 lambda_values = ['1024', '2048', '4096']
+lambda_markers = {'1024': 'o', '2048': 's', "4096": "d"}
 k = 256
 w = 0
+
+
+lambda_color = {'1024': 'red', '2048': 'blue', "4096": "green"}
+
+plt.plot([], [], color="black", ls="--", label="Raspberry Pi")
+plt.plot([], [], color="black", ls="-", label="i7-7820HQ")
+
 
 for lamda in lambda_values:
     display_value = []
@@ -14,10 +22,39 @@ for lamda in lambda_values:
         verif_time = []
 
         for line in f:
-            verif_time.append(float(line.split(';')[3]))
-        display_value.append(np.max(verif_time))
+            verif_time.append(1000 * float(line.split(';')[3]))
+        display_value.append(np.min(verif_time))
 
-    plt.plot(t_values, display_value)
+    plt.plot(t_values, display_value,
+             color=lambda_color[lamda], label="$k$=" + lamda, marker=lambda_markers[lamda])
+
+
+for lamda in lambda_values:
+    display_value = []
+    for t in t_values:
+        f = open("result_rp/" + str(t) + "_" + str(lamda) +
+                 "_" + str(k) + "_" + str(w) + ".csv")
+        verif_time = []
+
+        for line in f:
+            verif_time.append(1000 * float(line.split(';')[3]))
+        display_value.append(np.min(verif_time))
+
+    plt.plot(t_values, display_value, ls='dashed',
+             color=lambda_color[lamda], marker=lambda_markers[lamda])
+
+
 #plt.ylim([0, 0.0035])
-plt.yscale("log")
+plt.legend()
+plt.yscale('log')
+plt.grid(b=True, which='major', color='k', linestyle='-', axis='y')
+plt.grid(b=True, which='minor', color='lightgrey', linestyle='-', axis='y')
+
+plt.xlabel("Challenge - number of squarings")
+plt.ylabel("Verification time (ms)")
+
+plt.xticks([10, 11, 12, 13, 14], [
+           '$2^{10}$', '$2^{11}$', '$2^{12}$', '$2^{13}$', '$2^{14}$'])
+
+plt.savefig("log.pdf")
 plt.show()
