1616aac7b668de5ca11b37329cc7e075c7a12648
Vidal Attias <vidal.attias@gmail.com>
2020-05-19 00:13:18 +0200

    modifiying cerr

--- src/wesolowski.cpp
+++ src/wesolowski.cpp
@@ -76,7 +76,6 @@ Proof Wesolowski::evaluate(mpz_t l, mpz_t pi, const mpz_t x,
 
         eval_time = finish_eval - start_eval;
 
-        std::cout << eval_time.count() << std::endl;
 
         // WE FINISHED THE EVALUATION
 
